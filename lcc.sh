22f55402f9024b335db33dca2a796395620cb555
Adib Mosharrof <adib.mosharrof@gmail.com>
2022-04-04 19:08:56 -0400

    added sh file for slurm in lcc

---
+++ lcc.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#SBATCH --time=01:00:00 # Time limit for the job (REQUIRED).
+#SBATCH --job-name=my_test_job # Job name
+#SBATCH --ntasks=1 # Number of cores for the job. Same as SBATCH -n 1
+#SBATCH --partition=P4V12_SKY32M192_L # Partition/queue to runthe job in. (REQUIRED)
+#SBATCH -e lcc_out/slurm-%j.err # Error file for this job.
+#SBATCH -o lcc_out/slurm-%j.out # Output file for this job.
+#SBATCH -A gel_msi290_s22cs685 # Project allocation account name
+#SBATCH --gres=gpu:1
+
+module load ccs/Miniconda3
+source activate ./envs
+python train_intent.py
