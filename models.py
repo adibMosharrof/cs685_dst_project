73852269132ff3a17c9cdbd6a3101a713d0f4ef1
Adib Mosharrof <adib.mosharrof@gmail.com>
2022-05-01 15:59:47 -0400

    created forward function in model

--- models.py
+++ models.py
@@ -96,7 +96,6 @@ class BaseModel(pl.LightningModule):
         self.criterion = loss_func(model=self.model)
         self.train_metrics = metrics.MetricCollection(
             {
-                "train_top-01": metrics.Accuracy(),
                 "train_top-01": metrics.Accuracy(top_k=1),
                 "train_top-02": metrics.Accuracy(top_k=2),
                 "train_top-03": metrics.Accuracy(top_k=3),
@@ -105,7 +104,6 @@ class BaseModel(pl.LightningModule):
         )
         self.val_metrics = metrics.MetricCollection(
             {
-                "val_top-01": metrics.Accuracy(),
                 "val_top-01": metrics.Accuracy(top_k=1),
                 "val_top-02": metrics.Accuracy(top_k=2),
                 "val_top-03": metrics.Accuracy(top_k=3),
@@ -114,7 +112,6 @@ class BaseModel(pl.LightningModule):
         )
         self.test_metrics = metrics.MetricCollection(
             {
-                "test_top-01": metrics.Accuracy(),
                 "test_top-01": metrics.Accuracy(top_k=1),
                 "test_top-02": metrics.Accuracy(top_k=2),
                 "test_top-03": metrics.Accuracy(top_k=3),
@@ -137,7 +134,7 @@ class BaseModel(pl.LightningModule):
 
 class ContrastiveIntentModel(pl.LightningModule):
     def __init__(
-        self, model_name="distilbert-base-uncased", data_root="processed_data"
+        self, model_name="sentence-transformers/all-MiniLM-L6-v2", data_root="processed_data"
     ):
         super().__init__()
 
@@ -147,7 +144,6 @@ class ContrastiveIntentModel(pl.LightningModule):
         self.metrics = {}
         self.train_metrics = metrics.MetricCollection(
             {
-                "train_top-01": metrics.Accuracy(),
                 "train_top-01": metrics.Accuracy(top_k=1),
                 "train_top-02": metrics.Accuracy(top_k=2),
                 "train_top-03": metrics.Accuracy(top_k=3),
@@ -156,7 +152,6 @@ class ContrastiveIntentModel(pl.LightningModule):
         )
         self.val_metrics = metrics.MetricCollection(
             {
-                "val_top-01": metrics.Accuracy(),
                 "val_top-01": metrics.Accuracy(top_k=1),
                 "val_top-02": metrics.Accuracy(top_k=2),
                 "val_top-03": metrics.Accuracy(top_k=3),
@@ -165,7 +160,6 @@ class ContrastiveIntentModel(pl.LightningModule):
         )
         self.test_metrics = metrics.MetricCollection(
             {
-                "test_top-01": metrics.Accuracy(),
                 "test_top-01": metrics.Accuracy(top_k=1),
                 "test_top-02": metrics.Accuracy(top_k=2),
                 "test_top-03": metrics.Accuracy(top_k=3),
@@ -181,6 +175,13 @@ class ContrastiveIntentModel(pl.LightningModule):
             self.all_intents_dict[step] = {k: i for i, k in enumerate(intents)}
             self.all_intents[step] = list(self.all_intents_dict[step].keys())
 
+    def forward(self, utterances, intents, test=False):
+        intents_emb = self.model.encode(intents, convert_to_tensor=True)
+        if test:
+            intents_emb = intents_emb.to("cpu")
+        similarity = util.cos_sim(utterances["sentence_embedding"], intents_emb)
+        return similarity
+
     def training_step(self, batch, batch_idx=None):
         return self._shared_step(batch, step=Steps.train.name, metric=self.test_metrics)
 
@@ -193,8 +194,9 @@ class ContrastiveIntentModel(pl.LightningModule):
     def _shared_step(self, batch, batch_idx=None, step="train", metric=None):
         utterances, intents, intents_str = batch
         loss = self.criterion([utterances, intents], 1)
-        intents_emb = self.model.encode(self.all_intents[step], convert_to_tensor=True)
-        similarity = util.cos_sim(utterances["sentence_embedding"], intents_emb)
+        similarity = self(utterances, self.all_intents[step])
+        # intents_emb = self.model.encode(self.all_intents[step], convert_to_tensor=True)
+        # similarity = util.cos_sim(utterances["sentence_embedding"], intents_emb)
         labels = torch.tensor(
             [self.all_intents_dict[step][intent] for intent in intents_str],
             device=self.model.device,
@@ -209,7 +211,7 @@ class ContrastiveIntentModel(pl.LightningModule):
 class SlotNameModel(BaseModel):
     def __init__(
         self,
-        model_name="distilbert-base-uncased",
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
         data_root="processed_data",
         loss_func_name: str = "OnlineContrastiveLoss",
     ):
@@ -226,13 +228,20 @@ class SlotNameModel(BaseModel):
             self.slot_names_dict[step] = {slot: i for i, slot in enumerate(slots)}
             self.slot_names[step] = list(self.slot_names_dict[step].keys())
 
+    def forward(self, utterances, slot_names, test=False):
+        slot_name_emb = self.model.encode(slot_names, convert_to_tensor=True)
+        if test:
+            slot_name_emb = slot_name_emb.to("cpu")
+        similarity = util.cos_sim(utterances["sentence_embedding"], slot_name_emb)
+        return similarity
+
     def _shared_step(self, batch, batch_idx=None, step="train", metric=None):
         utterances, slots, slot_names_id = batch
-        slot_names_id = slot_names_id[0]
 
         loss = self.criterion([utterances, slots], 1)
-        slot_name_emb = self.model.encode(self.slot_names[step], convert_to_tensor=True)
-        similarity = util.cos_sim(utterances["sentence_embedding"], slot_name_emb)
+        # slot_name_emb = self.model.encode(self.slot_names[step], convert_to_tensor=True)
+        # similarity = util.cos_sim(utterances["sentence_embedding"], slot_name_emb)
+        similarity = self(utterances, self.slot_names[step])
         self.log_dict(metric(similarity, slot_names_id), prog_bar=True)
         return loss
 
